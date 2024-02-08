7f270a6c8c43ecafe2d7abd052a7ecf00b0aa4dc
Daniel <dpopov787@gmail.com>
2024-02-08 18:32:21 +0200

    setup api calls

---
+++ src/data/api.js
@@ -0,0 +1,42 @@
+const host = 'https://parseapi.back4app.com';
+const appId = 'DxlPXCpDR8DuVnkbBS4Hqop3w2WMX9tGV5C4Q0Q7';
+const apiKey = '4TLbwx5CsoxJlWnlDRUgVhWXYFE11vM1clfogiLe';
+
+async function request(method, url = '/', data) {
+  const options = {
+    method,
+    headers: {
+      'X-Parse-Application-Id': appId,
+      'X-Parse-JavaScript-Key': apiKey,
+    },
+  };
+
+  if (data !== undefined) {
+    options.headers['Content-Type'] = 'application/json';
+    options.body = JSON.stringify(data);
+  }
+
+  try {
+    const response = await fetch(host + url, options);
+
+    if (response.status === 204) {
+      return response;
+    }
+
+    const result = await response.json();
+
+    if (response.ok != true) {
+      console.log(result);
+      throw new Error(result.message || result.error);
+    }
+    return result;
+  } catch (error) {
+    alert(error.message);
+    throw error;
+  }
+}
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const del = request.bind(null, 'DELETE');
