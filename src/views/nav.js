d8ba2773ef5e6af86453d1fe0439010ebcca4081
Daniel <d.popov0470@gmail.com>
2024-02-25 21:05:02 +0200

    navigation is now responsive

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,17 +1,33 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-export const navTemplate = () => html`<nav>
-    <div>
-      <p class="title">Quiz<span>io</span></p>
-    </div>
+export const navTemplate = () => html` <div class="navigation-wrapper">
+    <p class="title">Quiz<span>io</span></p>
 
-    <div class="navigation-container">
+    <button class="expand-nav" @click="${showMobileNav}">
+      <i class="fa-solid fa-bars fa-4x" style="color: var(--primary);"></i>
+    </button>
+
+    <nav>
+      <button class="compress-nav" @click="${showMobileNav}">
+        <i class="fa-solid fa-x fa-3x" style="color: var(--primary);"></i>
+      </button>
       <a href="">Browse</a>
       <a href="">Create a quiz</a>
       <a href="">About us</a>
       <button class="white-btn">
         <a href="/login">Sign in</a>
       </button>
-    </div>
-  </nav>
+    </nav>
+  </div>
   <div class="border"></div>`;
+
+function showMobileNav() {
+  const nav = document.querySelector('nav');
+  if (nav.classList.contains('show-menu')) {
+    nav.classList.remove('show-menu');
+    nav.classList.add('hide-menu');
+  } else {
+    nav.classList.add('show-menu');
+    nav.classList.remove('hide-menu');
+  }
+}
