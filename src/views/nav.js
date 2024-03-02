87d3a769b382b6d755cd878355afb5e35fad2aa9
Daniel <d.popov0470@gmail.com>
2024-03-02 22:57:32 +0200

    rework nav

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,14 +1,24 @@
+import anime from '../../node_modules/animejs/lib/anime.es.js';
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
 export const navTemplate = () => html` <div class="navigation-wrapper">
-    <p class="title">Quiz<span>io</span></p>
+    <a href="/" class="title">Quiz<span>io</span></a> 
 
     <button class="expand-nav" @click="${showMobileNav}">
       <i class="fa-solid fa-bars fa-4x" style="color: var(--primary);"></i>
     </button>
 
     <nav>
-      <button class="compress-nav" @click="${showMobileNav}">
+      <a href="">Browse</a>
+      <a href="">Create a quiz</a>
+      <a href="">About us</a>
+      <button class="white-btn">
+        <a href="/login">Sign in</a>
+      </button>
+    </nav>
+
+    <nav class="mobile-nav">
+      <button class="close-nav" @click="${hideMobileNav}">
         <i class="fa-solid fa-x fa-3x" style="color: var(--primary);"></i>
       </button>
       <a href="">Browse</a>
@@ -22,12 +32,14 @@ export const navTemplate = () => html` <div class="navigation-wrapper">
   <div class="border"></div>`;
 
 function showMobileNav() {
-  const nav = document.querySelector('nav');
-  if (nav.classList.contains('show-menu')) {
-    nav.classList.remove('show-menu');
-    nav.classList.add('hide-menu');
-  } else {
-    nav.classList.add('show-menu');
-    nav.classList.remove('hide-menu');
-  }
+  anime({ targets: '.mobile-nav', translateY: 0 ,easing: 'easeInOutExpo' });
 }
+
+function hideMobileNav() {
+  anime({ targets: '.mobile-nav', translateY: '-100%' ,easing: 'easeInOutExpo' });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.mobile-nav');
+  nav.style.transform = 'translateY(-100%)';
+});
