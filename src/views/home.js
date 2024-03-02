8e70c78bfdabfee8d765620afd5678f317f14d5b
Daniel <d.popov0470@gmail.com>
2024-03-02 22:56:38 +0200

    add home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,21 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const homeTemplate = () => html`<h1>Hello world</h1>`;
+const homeTemplate = () => html`<div class="home-wrapper">
+<h1>Create, explore <br /> and expand your knowledge!</h1>
+
+<div class="sub-heading">
+<i class="fa-solid fa-grip-lines-vertical fa-2x"></i>
+  <p> 
+ Quiz your way to mastery with quizio!
+</p>
+</div>
+
+  <button class="yellow-btn">
+    <a href="">Sign up</a>
+  </button>
+
+</div> 
+<img src="../../static/images/home image.jpg" alt="" />`;
 
 export function renderHome(ctx) {
   ctx.render(homeTemplate());
