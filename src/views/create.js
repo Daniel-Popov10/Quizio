3853541b339144c8c8be722099797f4363fa922e
Daniel <d.popov0470@gmail.com>
2024-04-05 17:41:55 +0300

    add correct answer and delete answer styles to create page

--- src/views/create.js
+++ src/views/create.js
@@ -1,33 +1,39 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const createQuizTemplate = () => html`
-<div class="create-wrapper">
-    <form class="create-form">
+const createQuizTemplate = () => html` <div class="create-wrapper">
+  <form class="create-form">
     <p class="create-title">Create your quiz</p>
 
+    <div class="quiz-title">
+      <label for="title"></label>
+      <input type="text" name="title" id="title" placeholder="Title your quiz"/>
+    </div>
 
-<div class="quiz-title">
-        <label for="title"></label>
-        <input type="text" name="title" id="title" placeholder="Title your quiz"/>
-</div>
-       
-        <div class="question-container">
-        <div class="question">
-            <label for="question"></label>
-            <input type="text" name="question" id="question" placeholder="Question" />
-            </div>
-            <div class="answer">
-            <label for="answer"></label>
-            <input type="text" name="answer" id="answer" placeholder="New answer" />
-            </div>
-            <button class="yellow-btn delete-question">Delete question</button>
-        </div>
-
-        <button class="yellow-btn add-question">
-        <i class="fa-solid fa-plus"></i>
-            Click to add question
+    <div class="question-container">
+      <div class="question">
+        <label for="question"></label>
+        <input type="text" name="question" id="question" placeholder="Question"/>
+      </div>
+      <div class="answer">
+        <label for="answer"></label>
+        <input type="text" name="answer" id="answer" placeholder="New answer" />
+        <div class="correct-answer-container">
+        <label for="correct-answer"> Correct answer </label>
+        <input type="checkbox" name="correct-answer" id="correct-answer" />
+        <button class="delete-answer">
+        <i class="fa-solid fa-trash fa-lg" style="color: var(--primary);"></i>
         </button>
-    </form>
+      </div>
+      </div>
+
+      <button class="yellow-btn delete-question">Delete question</button>
+    </div>
+
+    <button class="yellow-btn add-question">
+      <i class="fa-solid fa-plus"></i>
+      Click to add question
+    </button>
+  </form>
 </div>`;
 
 export function renderCreate(ctx) {
