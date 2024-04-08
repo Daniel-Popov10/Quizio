c1676e437b11ca4e7a4a543e0312e1ad6e5901cb
Daniel <d.popov0470@gmail.com>
2024-04-08 20:23:07 +0300

    home page responsive changes

--- src/views/home.js
+++ src/views/home.js
@@ -1,21 +1,20 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
 const homeTemplate = () => html`<div class="home-wrapper">
-<h1>Create, explore <br /> and expand your knowledge!</h1>
-
-<div class="sub-heading">
-<i class="fa-solid fa-grip-lines-vertical fa-2x"></i>
-  <p> 
- Quiz your way to mastery with quizio!
-</p>
-</div>
-
+<div class="content">
+  <h1>Create, explore <br /> and expand your knowledge!</h1>
+  <div class="sub-heading">
+    <i class="fa-solid fa-grip-lines-vertical fa-2x"></i>
+    <p>Quiz your way to mastery with quizio!</p>
+  </div>
   <button class="yellow-btn">
     <a href="">Sign up</a>
   </button>
-
-</div> 
-<img src="../../static/images/home image.jpg" alt="" />`;
+</div>
+<div class="image-container">
+  <img src="../../static/images/home image.jpg" alt="" />
+</div>
+</div>`;
 
 export function renderHome(ctx) {
   ctx.render(homeTemplate());
