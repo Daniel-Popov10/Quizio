4e6eba3105f5e966855db1b4df5950f4a43b6ef4
Daniel <d.popov0470@gmail.com>
2024-04-11 17:58:38 +0300

    add utils for submit handler and pointer

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,16 @@ export function setUserData(userData) {
 export function clearUserData() {
     localStorage.removeItem('userData');
 }
+
+export function createPointer(className, objectId) {
+    return { __type: 'Pointer', className, objectId };
+}
+
+export function submitHandler(callback) {
+   return function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries([...formData].map(([k, v]) => [k, v.trim()]));
+    callback(data);
+   }
+}
